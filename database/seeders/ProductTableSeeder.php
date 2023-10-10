3441d06b8b4a3797c7112df19f3fa4006c01795b
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-10 19:08:41 +0200

    create seeders for user and product

---
+++ database/seeders/ProductTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+Use \App\Models\Product;
+
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::factory(25)->create();
+        Product::factory()->createOne([
+            'quantity' => 0
+        ]);
+    }
+}
