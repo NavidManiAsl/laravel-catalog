675a4697ad964622f4d699d241cd32c286c6315b
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-16 21:46:48 +0200

    Add home button to navbar and corrects the back button in detail view

--- resources/views/components/_header.blade.php
+++ resources/views/components/_header.blade.php
@@ -1,35 +1,34 @@
-<div 
-    x-data="{
-        open: false,
-        toggle() {
-            if (this.open) {
-                return this.close()
-            }
-
-            this.$refs.button.focus()
-
-            this.open = true
-        },
-        close(focusAfter) {
-            if (!this.open) return
-
-            this.open = false
-
-            focusAfter && focusAfter.focus()
+<div x-data="{
+    open: false,
+    toggle() {
+        if (this.open) {
+            return this.close()
         }
-    }" 
-    x-on:keydown.escape.prevent.stop="close($refs.button)"
-    x-on:focusin.window="! $refs.panel.contains($event.target) && close()" x-id="['dropdown-button']" class="relative"
->
-    
-    <div class="bg-gray-500 opacity-80 text-white h-20 fixed top-0 w-full" >
+
+        this.$refs.button.focus()
+
+        this.open = true
+    },
+    close(focusAfter) {
+        if (!this.open) return
+
+        this.open = false
+
+        focusAfter && focusAfter.focus()
+    }
+}" x-on:keydown.escape.prevent.stop="close($refs.button)"
+    x-on:focusin.window="! $refs.panel.contains($event.target) && close()" x-id="['dropdown-button']" class="relative">
+
+    <div class="bg-gray-500 opacity-80 text-white h-20 fixed top-0 w-full z-40 sm:block ">
         <nav class=" mx-auto py-4 flex justify-between items-center">
+            <a href="/products"><img src="{{asset('icons/home_icon.svg')}}" alt="home icon" class="ml-10 w-12"></a>
             <form action="/products/search" method="GET"
-                class="flex items-center space-x-2 flex-grow mx-22 max-w-5xl ml-20 mr-10 min-w-min hidden sm:flex">
+                class="flex items-center space-x-2 flex-grow mr-22 max-w-5xl ml-20 mr-10 min-w-min hidden sm:flex">
                 @csrf
                 <input type="text"
                     class="border border-gray-300 p-2 rounded-lg  focus:ring focus:ring-gray-800 text-black font-bold focus:outline-none
-                      flex-grow ">
+                      flex-grow "
+                    name="searchQuery">
                 <button type="submit"
                     class="bg-white  border-gray-500 font-bold py-2 px-4 rounded  flex-shrink-0 hover:bg-gray-400 transition duration-300">
                     <img src="{{ asset('icons/magnifying_glass.svg') }}" alt="Magnifying Glass Icon" class="w-6 h-6">
@@ -47,9 +46,9 @@
     </div>
     <div x-ref="panel" x-show="open" x-transition.origin.top x-on:click.outside="close($refs.button)"
         :id="$id('dropdown-button')" style="display: none;"
-        class="absolute left-0 w-full   rounded-md sm:hidden shadow-md">
-        <div class="bg-gray-500 opacity-80 text-white h-50 y">
-            <nav class="  py-4  px-4 flex  items-center flex-col">
+        class="absolute left-0 w-full z-40  rounded-md sm:hidden shadow-md">
+        <div class="bg-gray-500  text-white h-50 ">
+            <nav class="  py-4  px-4 flex  items-center flex-col sm:hidden">
                 <form action="/products/search" method="GET"
                     class="flex items-center justify-between space-x-1 flex-grow w-full mx-10 max-w-5xl min-w-min ">
                     @csrf
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,6 +8,6 @@
         <li><span class="font-bold">Description: </span>{{$product->description}}</li>
         <li><span class="font-bold">In stock: </span>{{$product->quantity}}</li>
         <li><span class="font-bold">Price: $</span>{{$product->price}}</li>
-        <a href="/products"><button class="border opacity-80 relative font-bold border-gray-950 px-4 py-2 rounded-md bg-slate-300 hover:bg-slate-500 ">Return</button></a>
+        <a href="{{ url()->previous() }}"><button class="border opacity-80 relative font-bold border-gray-950 px-4 py-2 rounded-md bg-slate-300 hover:bg-slate-500 ">Return</button></a>
     </ul>
 </div>
