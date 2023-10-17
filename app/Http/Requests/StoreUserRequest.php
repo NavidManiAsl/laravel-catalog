0b1bcc3d1c55199ed875b04cd254a2d6dcaae035
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-17 20:18:36 +0200

    create form request for the user class

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'min:3', 'max:256', 'regex:/^[a-zA-Z0-9_]+$/'],
+            'email' => ['required', 'email:rfc'],
+            'password' => ['required', 'regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/', 'confirmed']
+        ];
+    }
+}
