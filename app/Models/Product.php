2ec51d068031c3eed53f281cdeb5d09a0ab01f38
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-18 22:14:25 +0200

    Delete the getters and setters methods

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,42 +16,6 @@ class Product extends Model
     private int $price;
     private int $quantity;
 
-    public function getName(): string
-    {
-        return $this->productName;
-    }
 
-    public function getId(): string
-    {
-        return $this->productDescription;
-    }
-
-    public function getPrice(): int
-    {
-        return $this->price;
-    }
-
-
-    public function getQuantity(): int
-    {
-        return $this->quantity;
-    }
-
-    public function setName(string $newName): void
-    {
-        $this->productName = $newName;
-    }
-    public function setId(string $newDescription): void
-    {
-        $this->productDescription = $newDescription;
-    }
-    public function setPrice(int $newPrice): void
-    {
-        $this->price = $newPrice;
-    }
-    public function setQuantity(int $newQuantity): void
-    {
-        $this->quantity = $newQuantity;
-    }
 
 }
