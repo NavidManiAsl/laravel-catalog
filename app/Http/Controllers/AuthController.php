fb97b7ee0473567c3d4ffcfc6bafda1675e18202
NavidManiAsl <navid.maniasl@outlook.com>
2023-10-18 15:11:07 +0200

    Create the login and logout method for auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,27 +3,66 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
+
+    private $auth;
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
     /**
      * Display the login form.
      */
-    public function index () {
+    public function index()
+    {
         try {
-           
+
             return view('auth.index');
-            
+
         } catch (\Throwable $th) {
-           
-            Log::error('unexpected error: '. $th->getMessage(),[$th->getTrace()]);
+
+            Log::error('unexpected error: ' . $th->getMessage(), [$th->getTrace()]);
+            return redirect()->back()->with('error', 'Unexpected error. Try again later!');
         }
     }
 
-    public function login(LoginRequest $request) 
+    /**
+     * login a user based on entered credentials.
+     */
+    public function login(LoginRequest $request)
     {
-        dd($request);
+        $credentials = $request->only('username', 'password');
+        try {
+            if ($this->auth->attempt($credentials)) {
+
+                return redirect('/products');
+
+            } else {
+
+                return back()->with('error', 'We cant verify your credentials!');
+            }
+        } catch (\Throwable $th) {
+
+            Log::error('Login error: ' . $th->getMessage(), [$th->getTrace()]);
+            return redirect()->back()->with('error', 'Unexpected error. Try again later!');
+        }
+
+    }
+
+    public function logout(Guard $auth)
+    {
+        try {
+            $this->auth->logout();
+
+            return redirect('/products');
+        } catch (\Throwable $th) {
+            Log::error('Logout error: ' . $th->getMessage(), [$th->getTrace()]);
+            return redirect()->back()->with('error', 'Unexpected error. Try again later!');
+        }
+
     }
 }
